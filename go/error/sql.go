20d94a01e17181807660f48b6a983ea7cee47954
wujunjie <ajeopen@163.com>
2021-04-27 00:19:55 +0800

    wrap error

---
+++ go/error/sql.go
@@ -0,0 +1,12 @@
+func FindNickName(playerid int64) (nickName string, err error) {
+	err := db.QueryRow("select nick_name from users where id = ?", playerid).Scan(&nickName)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", errors.Wrapf(err, "FindNickName(%d)", playerid)
+		} else {
+			...
+		}
+	}
+	
+	return nickName, nil
+}
