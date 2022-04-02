42efd99395a6529e2b42279dfe28e6d447bf12ea
AJEOpen <48944129+AJEOpen@users.noreply.github.com>
2022-04-02 23:08:13 +0800

    Update sql.go

--- go/error/sql.go
+++ go/error/sql.go
@@ -2,7 +2,7 @@ func FindNickName(playerid int64) (nickName string, err error) {
 	err := db.QueryRow("select nick_name from users where id = ?", playerid).Scan(&nickName)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return "", errors.Wrapf(err, "FindNickName(%d)", playerid)
+			return "", errors.Wrapf(err, "FindNickName(playerid:%d)", playerid)
 		} else {
 			...
 		}
